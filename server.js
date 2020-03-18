35a8b4a37050e6106b5ae1e0bd00dcec5fb38124
umesh1987 <umesh.state@gmail.com>
2020-03-17 23:47:39 -0500

    add dependencies

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// require dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,13 +17,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
